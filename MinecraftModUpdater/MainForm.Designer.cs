fd77fc74b15c05aad83c7250567969f03d095dea
kekstm989 <68967660+kekstm989@users.noreply.github.com>
2025-02-14 22:14:10 +0300

    MainForm.Designer.cs (Добавлена кнопка "Обновить лаунчер")

--- MinecraftModUpdater/MainForm.Designer.cs
+++ MinecraftModUpdater/MainForm.Designer.cs
@@ -2,11 +2,14 @@
 {
     partial class MainForm
     {
+        private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.Button btnUpdateMods;
+        private System.Windows.Forms.Button btnUpdateLauncher;
 
         private void InitializeComponent()
         {
             this.btnUpdateMods = new System.Windows.Forms.Button();
+            this.btnUpdateLauncher = new System.Windows.Forms.Button();
             this.SuspendLayout();
 
             // 
@@ -21,10 +24,22 @@
             this.btnUpdateMods.Click += new System.EventHandler(this.btnUpdateMods_Click);
 
             // 
+            // btnUpdateLauncher
+            // 
+            this.btnUpdateLauncher.Location = new System.Drawing.Point(50, 70);
+            this.btnUpdateLauncher.Name = "btnUpdateLauncher";
+            this.btnUpdateLauncher.Size = new System.Drawing.Size(150, 30);
+            this.btnUpdateLauncher.TabIndex = 1;
+            this.btnUpdateLauncher.Text = "Обновить лаунчер";
+            this.btnUpdateLauncher.UseVisualStyleBackColor = true;
+            this.btnUpdateLauncher.Click += new System.EventHandler(this.btnUpdateLauncher_Click);
+
+            // 
             // MainForm
             // 
-            this.ClientSize = new System.Drawing.Size(250, 100);
+            this.ClientSize = new System.Drawing.Size(250, 120);
             this.Controls.Add(this.btnUpdateMods);
+            this.Controls.Add(this.btnUpdateLauncher);
             this.Name = "MainForm";
             this.Text = "Minecraft Mod Updater";
             this.ResumeLayout(false);
