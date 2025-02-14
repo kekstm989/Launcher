5fdc077da405c3fc132832029c68301c2ab79a21
kekstm989 <68967660+kekstm989@users.noreply.github.com>
2025-02-14 22:13:43 +0300

    MainForm.cs (Добавлена кнопка обновления)

--- MinecraftModUpdater/MainForm.cs
+++ MinecraftModUpdater/MainForm.cs
@@ -16,5 +16,18 @@ namespace MinecraftModUpdater
             await ModUpdater.UpdateModsAsync();
             MessageBox.Show("Обновление завершено!", "Minecraft Mod Updater", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+
+        private async void btnUpdateLauncher_Click(object sender, EventArgs e)
+        {
+            bool isUpdateAvailable = await Updater.CheckForUpdateAsync();
+            if (isUpdateAvailable)
+            {
+                await Updater.UpdateLauncherAsync();
+            }
+            else
+            {
+                MessageBox.Show("У вас уже последняя версия.", "Обновление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
     }
 }
